ad17cbf65182c9b4c86e694524b0186a4d615d5c
Nathan Lenkowski <nathan@littlebiglab.com>
2018-02-14 13:01:29 +0100

    Add Task routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
+// Get task model
+use App\Task;
+
 Route::get('/', function () {
     return view('welcome');
+// Show all tasks
+Route::get('/tasks', function () {
+
+    $tasks = App\Task::incomplete();
+    //$tasks = DB::table('tasks')->latest()->get();
+
+    return view('tasks.index', compact('tasks'));
+});
+
+// Show single task
+Route::get('/tasks/{task}', function ($id) {
+
+    $task = App\Task::find($id);
+    //$task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
 });
